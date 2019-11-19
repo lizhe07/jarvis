51ee5035b49a361e62acaf1ce5683438ac026e0b
Zhe Li <zhe.li@bcm.edu>
2019-11-18 23:02:18 -0600

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 18 20:51:54 2019
+
+@author: Zhe
+"""
+
+from setuptools import setup, find_packages
+setup(
+    name="jarvis",
+    version="0.0.1",
+    author='Zhe Li',
+    python_requires='>=3',
+    packages=find_packages(),
+)
