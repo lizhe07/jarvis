8a777ee86b07507aac539d07085ba93948c85444
Zhe Li <zhe.li@bcm.edu>
2021-01-28 00:48:58 -0600

    Update prepare_datasets
    Codes are simplified, while options are reduced to 'MNIST', 'CIFAR10', 'CIFAR100' for now.

--- jarvis/vision.py
+++ jarvis/vision.py
@@ -5,8 +5,7 @@ Created on Fri Jul 31 21:59:48 2020
 @author: Zhe
 """
 
-import os, random
-import torch, torchvision
+import random, torch, torchvision
 import numpy as np
 from torch.utils.data import Subset, DataLoader
 from torchvision import transforms
@@ -21,151 +20,83 @@ MODELS = {
     'ResNet152': resnet.resnet152,
     }
 
+DEFAULT_IMAGE_AUG = lambda size: [
+    transforms.Pad(4, padding_mode='reflect'),
+    transforms.RandomRotation(2),
+    transforms.RandomCrop(size),
+    transforms.RandomHorizontalFlip(),
+    transforms.ColorJitter(.25, .25, .25),
+    ]
+DEFAULT_DIGIT_AUG = lambda size: [
+    transforms.Pad(4, padding_mode='reflect'),
+    transforms.RandomRotation(2),
+    transforms.RandomCrop(size),
+    ]
 
-def prepare_datasets(task, datasets_dir, valid_num=None, *,
-                     grayscale=False, t_aug=None):
+# (dataset, t_aug, class_num, sample_num) for different datasets
+DATASETS_META = {
+    'MNIST': (torchvision.datasets.MNIST, DEFAULT_DIGIT_AUG(28), 10, 60000),
+    'CIFAR10': (torchvision.datasets.CIFAR10, DEFAULT_IMAGE_AUG(32), 10, 50000),
+    'CIFAR100': (torchvision.datasets.CIFAR100, DEFAULT_IMAGE_AUG(32), 100, 50000),
+    }
+
+
+def prepare_datasets(task, datasets_dir, split_ratio=None,
+                     augment=True, grayscale=False):
     r"""Prepares vision datasets.
 
     Args
     ----
     task: str
-        The name of the dataset, e.g. ``'CIFAR10'``.
+        The name of the datasets, e.g. ``'CIFAR10'``.
     datasets_dir: str
-        The directory of vision benchmarks.
-    valid_num: int
-        The validation dataset size. For ``'16ImageNet'`` task, it is the
-        number of validation images per class. When `valid_num` is ``None``,
-        only testing dataset will be returned.
+        The directory of the datasets. Automatic download is disabled.
+    split_ratio: float
+        The ratio of training set, usually `0.9` or `0.8`. When `split_ratio`
+        is ``None``, only testing set will be returned.
+    augment: bool
+        Whether to apply data augmentation on training set.
     grayscale: bool
         Whether to use grayscale images.
-    t_aug: transform
-        Data augmentation transformation.
 
     Returns
     -------
     dataset_train, dataset_valid, dataset_test: Dataset
-        The training/validation/testing datasets for the task pathway. Each
+        The training/validation/testing datasets for training models. Each
         dataset item is a tuple `(image, label)`. `image` is a float tensor of
-        shape `(3, H, W)` in [0, 1], and `label` is an integer.
-    weight: (class_num,), array_like
-        The class weight for training dataset. If the classes are balanced,
-        ``None`` is returned.
+        shape `(3, H, W)` or `(1, H, W)` in [0, 1], and `label` is an integer.
 
     Examples
     --------
     >>> dataset_test = prepare_datasets(task, datasets_dir)
 
-    >>> dataset_train, dataset_valid, dataset_test, weight = \
-            prepare_datasets(task, datasets_dir, valid_num)
+    >>> dataset_train, dataset_valid, dataset_test = prepare_datasets(
+            task, datasets_dir, split_ratio
+            )
 
     """
-    t_test = [transforms.ToTensor()]
     if grayscale:
-        t_test = [transforms.Grayscale()]+t_test
+        t_test = [transforms.Grayscale(), transforms.ToTensor()]
+    else:
+        t_test = [transforms.ToTensor()]
+    dataset, t_aug, _, sample_num = DATASETS_META[task]
+    if augment:
+        t_train = t_aug+t_test
+    else:
+        t_train = t_test
+    t_test = transforms.Compose(t_test)
+    t_train = transforms.Compose(t_train)
 
-    if t_aug is None:
-        if task=='MNIST':
-            t_aug = [transforms.RandomCrop(28, padding=4)]
-        if task.startswith('CIFAR'):
-            t_aug = [
-                transforms.RandomCrop(32, padding=4, padding_mode='reflect'),
-                transforms.RandomHorizontalFlip()
-                ]
-        if task=='ImageNet':[
-                transforms.RandomResizedCrop(224),
-                transforms.RandomHorizontalFlip(),
-                ]
-    t_train = t_aug+t_test
+    dataset_test = dataset(datasets_dir, train=False, transform=t_test)
+    if split_ratio is None:
+        return dataset_test
 
-    if task=='MNIST':
-        t_test = transforms.Compose(t_test)
-        dataset_test = torchvision.datasets.MNIST(
-            datasets_dir, train=False, transform=t_test,
-            )
-
-        if valid_num is None:
-            return dataset_test
-        else:
-            t_train = transforms.Compose(t_train)
-            sample_num = 60000
-            idxs_valid = np.array(random.sample(range(sample_num), valid_num))
-            idxs_train = np.setdiff1d(np.arange(sample_num), idxs_valid, assume_unique=True)
-            dataset_train = Subset(torchvision.datasets.MNIST(
-                datasets_dir, train=True, transform=t_train,
-                ), idxs_train)
-            dataset_valid = Subset(torchvision.datasets.MNIST(
-                datasets_dir, train=True, transform=t_test,
-                ), idxs_valid)
-
-            weight = None
-            return dataset_train, dataset_valid, dataset_test, weight
-
-    if task.startswith('CIFAR'):
-        t_test = transforms.Compose(t_test)
-        if task=='CIFAR10':
-            dataset_test = torchvision.datasets.CIFAR10(
-                datasets_dir, train=False, transform=t_test,
-                )
-        if task=='CIFAR100':
-            dataset_test = torchvision.datasets.CIFAR100(
-                datasets_dir, train=False, transform=t_test,
-                )
-
-        if valid_num is None:
-            return dataset_test
-        else:
-            t_train = transforms.Compose(t_train)
-            sample_num = 50000
-            idxs_valid = np.array(random.sample(range(sample_num), valid_num))
-            idxs_train = np.setdiff1d(np.arange(sample_num), idxs_valid, assume_unique=True)
-            if task=='CIFAR10':
-                dataset_train = Subset(torchvision.datasets.CIFAR10(
-                    datasets_dir, train=True, transform=t_train,
-                    ), idxs_train)
-                dataset_valid = Subset(torchvision.datasets.CIFAR10(
-                    datasets_dir, train=True, transform=t_test,
-                    ), idxs_valid)
-            if task=='CIFAR100':
-                dataset_train = Subset(torchvision.datasets.CIFAR100(
-                    datasets_dir, train=True, transform=t_train,
-                    ), idxs_train)
-                dataset_valid = Subset(torchvision.datasets.CIFAR100(
-                    datasets_dir, train=True, transform=t_test,
-                    ), idxs_valid)
-
-            weight = None
-            return dataset_train, dataset_valid, dataset_test, weight
-
-    if task=='ImageNet':
-        t_test = transforms.Compose(
-            [transforms.Resize(256), transforms.CenterCrop(224)]+t_test
-            )
-        dataset_test = torchvision.datasets.ImageFolder(
-            f'{datasets_dir}/ILSVRC2012/val', transform=t_test,
-            )
-
-        if valid_num is None:
-            return dataset_test
-        else:
-            t_train = transforms.Compose(t_train)
-            class_names = [c for c in os.listdir(f'{datasets_dir}/ILSVRC2012/train') if c.startswith('n')]
-            sample_nums = [len(os.listdir(f'{datasets_dir}/ILSVRC2012/train/{c_name}')) \
-                           for c_name in class_names]
-            idxs_valid = [random.sample(range(sample_num), valid_num) \
-                          for sample_num in sample_nums]
-            idxs_valid = np.concatenate([
-                np.array(idxs_valid[i])+sum(sample_nums[:i]) for i in range(len(class_names))
-                ])
-            idxs_train = np.setdiff1d(np.arange(sum(sample_nums)), idxs_valid, assume_unique=True)
-            dataset_train = Subset(torchvision.datasets.ImageFolder(
-                f'{datasets_dir}/ILSVRC2012/train', transform=t_train,
-                ), idxs_train)
-            dataset_valid = Subset(torchvision.datasets.ImageFolder(
-                f'{datasets_dir}/ILSVRC2012/train', transform=t_test,
-                ), idxs_valid)
-
-            weight = None
-            return dataset_train, dataset_valid, dataset_test, weight
+    assert split_ratio>0 and split_ratio<1
+    idxs_train = np.array(random.sample(range(sample_num), int(sample_num*split_ratio)))
+    idxs_valid = np.setdiff1d(np.arange(sample_num), idxs_train, assume_unique=True)
+    dataset_train = Subset(dataset(datasets_dir, train=True, transform=t_train), idxs_train)
+    dataset_valid = Subset(dataset(datasets_dir, train=True, transform=t_test), idxs_valid)
+    return dataset_train, dataset_valid, dataset_test
 
 
 def prepare_model(task, arch, **kwargs):
@@ -175,20 +106,21 @@ def prepare_model(task, arch, **kwargs):
     ----
     task: str
         The name of the dataset, e.g. ``'CIFAR10'``.
-    arch: str
-        The name of model architecture, e.g. ``ResNet18``.
+    arch: str or callable
+        The name of model architecture, e.g. ``ResNet18``, or a function that
+        returns a model object.
+
+    Returns
+    -------
+    model: nn.Module
+        A pytorch model that can be called by `logits = model(images)`.
 
     """
-    if task=='CIFAR10':
-        class_num = 10
-    if task=='CIFAR100':
-        class_num = 100
-    if task=='16ImageNet':
-        class_num = 16
-    if task=='ImageNet':
-        class_num = 1000
-
-    model = MODELS[arch](class_num=class_num, **kwargs)
+    _, _, class_num, _ = DATASETS_META[task]
+    if isinstance(arch, str):
+        model = MODELS[arch](class_num=class_num, **kwargs)
+    else:
+        model = arch(class_num=class_num, **kwargs)
     return model
 
 
