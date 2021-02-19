a35bca6cf371e9ace09595c5494dfab5d2136b73
Zhe Li <zhe.li@bcm.edu>
2021-02-19 02:46:01 -0600

    Update LeNet
    Both pre- and post-activations are returned.

--- jarvis/models/lenet.py
+++ jarvis/models/lenet.py
@@ -5,7 +5,7 @@ Created on Thu Feb 11 14:16:01 2021
 @author: Zhe
 """
 
-from typing import List, Any
+from typing import List, Tuple, Any
 
 import torch
 import torch.nn as nn
@@ -22,36 +22,38 @@ class LeNet(ImageClassifier):
 
     def __init__(
             self,
+            act_fun: str = 'ReLU',
             **kwargs: Any,
             ) -> None:
         super(LeNet, self).__init__(**kwargs)
         in_channels, class_num = self.in_channels, self.class_num
 
+        if act_fun=='ReLU':
+            self.act_fun = nn.ReLU()
+        else:
+            raise RuntimeError(f"activation function '{act_fun}' not recognized")
         self.layers = nn.ModuleList([
+            nn.Conv2d(in_channels, 6, kernel_size=5, padding=2),
             nn.Sequential(
-                nn.Conv2d(in_channels, 6, kernel_size=5, padding=2),
-                nn.ReLU(),
                 nn.MaxPool2d(kernel_size=2),
-                ),
-            nn.Sequential(
                 nn.Conv2d(6, 16, kernel_size=5, padding=2),
-                nn.ReLU(),
-                nn.MaxPool2d(kernel_size=2),
                 ),
             nn.Sequential(
+                nn.MaxPool2d(kernel_size=2),
                 nn.Conv2d(16, 120, kernel_size=5, padding=2),
-                nn.ReLU(),
+                ),
+            nn.Sequential(
                 nn.AdaptiveMaxPool2d(1),
                 nn.Flatten(1),
-                ),
-            nn.Sequential(
                 nn.Linear(120, 84),
-                nn.ReLU(),
                 ),
             ])
         self.fc = nn.Linear(84, out_features=class_num)
 
-    def layer_activations(self, x: torch.Tensor) -> List[torch.Tensor]:
+    def layer_activations(
+            self,
+            x: torch.Tensor
+            ) -> Tuple[List[torch.Tensor], List[torch.Tensor], torch.Tensor]:
         r"""Returns activations of all layers.
 
         Args
@@ -61,18 +63,20 @@ class LeNet(ImageClassifier):
 
         Returns
         -------
-        acts: list of tensors
-            The activations for each layer (unitl the last fc layer).
+        pre_acts, post_acts: list of tensors
+            The pre- and post-activations for each layer except the last one.
         logits: tensor
             The logits.
 
         """
-        acts = []
+        pre_acts, post_acts = [], []
         for layer in self.layers:
             x = layer(x)
-            acts.append(x)
+            pre_acts.append(x)
+            x = self.act_fun(x)
+            post_acts.append(x)
         logits = self.fc(x)
-        return acts, logits
+        return pre_acts, post_acts, logits
 
 
 def lenet(**kwargs: Any) -> LeNet:
