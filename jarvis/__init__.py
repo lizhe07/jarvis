488c64731e3b63330a75f3c38ac1daa910fc1840
Zhe Li <zhe.li@bcm.edu>
2022-04-24 23:24:10 -0500

    Bump version to 0.4.0

--- jarvis/__init__.py
+++ jarvis/__init__.py
@@ -1,11 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Aug 28 09:16:03 2019
-
-@author: zhe
-"""
-
-__version__ = '0.3.1'
+__version__ = '0.4.0'
 
 from .archive import Archive
 from .job import BaseJob
