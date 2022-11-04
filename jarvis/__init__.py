33aa187f611c7fef49cf89196c4bcd71db2c81ac
Zhe Li <zhe.li@bcm.edu>
2022-11-04 14:47:31 -0500

    Update version citing

--- jarvis/__init__.py
+++ jarvis/__init__.py
@@ -1,4 +1,5 @@
-__version__ = "0.5.2"
+with open('version.txt', 'r') as f:
+    __version__ = f.readline().split('"')[1]
 
 from .config import Config
 from .archive import Archive
--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
+with open('jarvis/version.txt', 'r') as f:
+    __version__ = f.readline().split('"')[1]
+
 setup(
     name="jarvis",
-    version="0.5.1",
+    version=__version__,
     author='Zhe Li',
     python_requires='>=3.9',
     packages=find_packages(),
