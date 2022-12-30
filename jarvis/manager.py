05018c33c6cb57e7199df0ffe2b2b9de8e152706
Zhe Li <zhel@bcm.edu>
2022-12-30 12:21:28 -0600

    Minor update

--- jarvis/manager.py
+++ jarvis/manager.py
@@ -290,8 +290,8 @@ class Manager:
 
                 if self.verbose>0:
                     print("------------")
-                    print("Processing {} ({})...".format(
-                        key, progress_str(w_count+1, count) if count>0 else w_count,
+                    print("Processing {} (work {})...".format(
+                        key, progress_str(w_count+1, count) if count>0 else w_count+1,
                     ))
                 self.process(config, num_epochs, resume)
                 w_count += 1
