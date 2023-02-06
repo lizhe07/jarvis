cc7820cd689a429a43f630aa10f368f291908d68
Zhe Li <zhe.li@bcm.edu>
2023-02-06 14:27:56 -0600

    Add basic testing for Config

---
+++ tests/test_config.py
@@ -0,0 +1,53 @@
+import unittest, yaml
+from pathlib import Path
+
+from jarvis.config import Config
+from jarvis.models.resnet import ResNet
+
+
+class TestConfig(unittest.TestCase):
+
+    def setUp(self):
+        self.config_path = Path(__file__).parent/'fixtures'/'resnet_config.yaml'
+
+    def test_nesting(self):
+        config = Config({
+            'a.b': 1,
+            'a.c': True,
+        })
+        # check nesting structure
+        self.assertEqual(set(config.keys()), set(['a']))
+        self.assertEqual(set(config['a'].keys()), set(['b', 'c']))
+        self.assertEqual(config['a']['b'], 1)
+        self.assertEqual(config['a.b'], 1)
+        # check value assignment
+        config['a.d'] = -3.14
+        self.assertEqual(set(config['a'].keys()), set(['b', 'c', 'd']))
+        self.assertEqual(config['a']['d'], -3.14)
+        config['a.d'] = 2.718
+        self.assertEqual(config['a']['d'], 2.718)
+        # check update method
+        config.update({'b.a': 'John'})
+        self.assertEqual(set(config.keys()), set(['a', 'b']))
+        self.assertEqual(config['b']['a'], 'John')
+
+    def test_dot_notation(self):
+        with open(self.config_path, 'r') as f:
+            config = Config(yaml.safe_load(f))
+        # test read
+        self.assertIsInstance(config.train.optimizer, Config)
+        # test write
+        config.data = 'CIFAR100'
+        config.train.optimizer.weight_decay = 1e-4
+        self.assertEqual(config.data, 'CIFAR100')
+        self.assertEqual(config.train.optimizer.weight_decay, 1e-4)
+
+    def test_instantiation(self):
+        with open(self.config_path, 'r') as f:
+            config = Config(yaml.safe_load(f))
+        model = config.model.instantiate()
+        self.assertIsInstance(model, ResNet)
+
+
+if __name__=='__main__':
+    unittest.main()
