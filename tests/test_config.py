a449a194f09432319b4088243ecdaad72c793805
Zhe Li <zhel@bcm.edu>
2024-05-22 15:50:18 -0500

    Update test for Config class

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,14 +2,12 @@ import unittest, yaml
 from pathlib import Path
 
 from jarvis.config import Config
-from jarvis.models.resnet import ResNet
 
 
 class TestConfig(unittest.TestCase):
 
     def setUp(self):
-        self.config_path = Path(__file__).parent/'fixtures'/'resnet_config.yaml'
-        self.defaults_path = Path(__file__).parent/'fixtures'/'defaults.yaml'
+        self.config_path = Path(__file__).parent/'fixtures/resnet.yaml'
 
     def test_nesting(self):
         config = Config({
@@ -32,27 +30,6 @@ class TestConfig(unittest.TestCase):
         self.assertEqual(set(config.keys()), set(['a', 'b']))
         self.assertEqual(config['b']['a'], 'John')
 
-    def test_fill(self):
-        config = Config({
-            'data': 'CIFAR100',
-        })
-        config.fill(self.config_path)
-        config.lookup(self.defaults_path)
-        self.assertEqual(set(config.keys()), set(['data', 'model', 'train']))
-        self.assertEqual(config.data, 'CIFAR100')
-        self.assertEqual(
-            set(config.model.keys()),
-            set(['_target_', 'base_channels', 'conv0_channels']),
-        )
-        self.assertEqual(config.model.base_channels, 64)
-        self.assertEqual(config.model.conv0_channels, 32)
-        self.assertEqual(
-            set(config.train.keys()),
-            set(['pretrained', 'batch_size', 'optimizer'])
-        )
-        self.assertEqual(config.train.optimizer.lr, 1e-4)
-        self.assertEqual(config.train.optimizer.momentum, 0.99)
-
     def test_dot_notation(self):
         with open(self.config_path, 'r') as f:
             config = Config(yaml.safe_load(f))
@@ -63,12 +40,6 @@ class TestConfig(unittest.TestCase):
         config.train.optimizer.weight_decay = 1e-4
         self.assertEqual(config.data, 'CIFAR100')
         self.assertEqual(config.train.optimizer.weight_decay, 1e-4)
-
-    def test_instantiation(self):
-        with open(self.config_path, 'r') as f:
-            config = Config(yaml.safe_load(f))
-        model = config.model.instantiate()
-        self.assertIsInstance(model, ResNet)
 
 
 if __name__=='__main__':
